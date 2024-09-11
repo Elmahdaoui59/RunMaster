8ec006b6f333b1510af139e37853fbd177f041c0
Elmahdaoui <abdelghani.elmahdaoui@adria-bt.com>
2024-09-11 15:34:41 +0100

    Creating the Material Theme

--- app/src/main/java/com/running/runmaster/MainActivity.kt
+++ app/src/main/java/com/running/runmaster/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.running.runmaster.ui.theme.RunMasterTheme
+import com.running.core.presentation.designsystem.RunMasterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
