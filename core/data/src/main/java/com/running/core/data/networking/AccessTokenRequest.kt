32209a6c52f682b82e7b8b00c4fc4d9c31eb52dd
abdelghani <abdelghani.payment@gmail.com>
2024-09-22 22:38:47 +0100

    6.7 Setting up the token refresh mechanism

---
+++ core/data/src/main/java/com/running/core/data/networking/AccessTokenRequest.kt
@@ -0,0 +1,9 @@
+package com.running.core.data.networking
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenRequest(
+    val refreshToken: String,
+    val userId: String
+)
