5ddd2eb34c682e522d2bf47817a9c27078acf429
abdelghani <abdelghani.payment@gmail.com>
2024-10-01 00:33:47 +0100

    7.7 setting up the running tracker

---
+++ run/domain/src/main/java/com/running/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,17 @@
+package com.running.run.domain
+
+import com.running.core.domain.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timestampsPerLine ->
+                timestampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+                }.sum().roundToInt()
+            }
+    }
+}
+
--- run/domain/src/main/java/com/running/run/domain/RunningTracker.kt
+++ run/domain/src/main/java/com/running/run/domain/RunningTracker.kt
@@ -1,19 +1,41 @@
 package com.running.run.domain
 
+import com.running.core.domain.Timer
+import com.running.core.domain.location.LocationTimestamp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combineTransform
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.zip
+import kotlin.math.roundToInt
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RunningTracker(
     private val locationObserver: LocationObserver,
     private val applicationScope: CoroutineScope
 ) {
+    private val _runData = MutableStateFlow(RunData())
+    val runData = _runData.asStateFlow()
 
+    private val isTracking = MutableStateFlow(false)
     private val isObservingLocation = MutableStateFlow(false)
+
+    private val _elapsedTime = MutableStateFlow(Duration.ZERO)
+    val elapsedTime = _elapsedTime.asStateFlow()
+
+
     val currentLocation = isObservingLocation
         .flatMapLatest { isObservingLocation ->
             if (isObservingLocation) {
@@ -26,6 +48,81 @@ class RunningTracker(
             null
         )
 
+    init {
+        isTracking
+            // flatmapLatest just map a flow to another flow
+            .flatMapLatest { isTracking ->
+                //every time 'isTracking' switches from true to false or the inverse,
+                // if true we trigger our timer again and listen to timer updates
+                if (isTracking) {
+                    Timer.timeAndEmit()
+                    //if false(not tracking) we map that to an empty flow(will not emit)
+                } else flowOf()
+            }
+            //// and for each of this updates, we add that to our current elapsed time
+            .onEach {
+                _elapsedTime.value += it
+            }
+            .launchIn(applicationScope)
+
+        currentLocation
+            .filterNotNull()
+            //combineTransform is used here(instead of 'combine()') because it give us a FlowCollector so we can call 'emit' in the
+            //new flow.
+            .combineTransform(isTracking) { location, isTracking ->
+                //the 'isTracking' flow is combined with the 'currentLocation' flow, whenever one
+                // of the two changes, this block of code is fired again.
+                if (isTracking) {
+                    emit(location)
+                }
+            }
+                /*
+            .onEach {
+                //this block with be called every time we have a non null location and we are currently tracking
+            }
+                 */
+            .zip(_elapsedTime) { location, elapsedTime ->
+                // 'zip()' operator will work similarly to combine, but this block of code will be fired
+                // only if there are emission of both flows
+                // The '_elapsedTime' emit more frequently than location flow. So it will block until
+                // there is emission of the location flow. And will pair the location with the most
+                // up to date elapsed tine
+                LocationTimestamp(
+                    location = location,
+                    durationTimestamp = elapsedTime
+                )
+            }
+            .onEach { location ->
+                val currentLocations = runData.value.locations
+                val lastLocationsList = if (currentLocations.isNotEmpty()) {
+                    currentLocations.last() + location
+                } else listOf(location)
+
+                val newLocationsList = currentLocations.replaceLast(lastLocationsList)
+                val distanceMeters = LocationDataCalculator.getTotalDistanceMeters(
+                    locations = newLocationsList
+                )
+                val distanceKm = distanceMeters / 1000.0
+                val currentDuration = location.durationTimestamp
+                val avgSecondPerKm = if (distanceKm == 0.0) {
+                    0
+                } else {
+                    (currentDuration.inWholeSeconds / distanceKm).roundToInt()
+                }
+                _runData.update {
+                    RunData(
+                        distanceMeters = distanceMeters,
+                        pace = avgSecondPerKm.seconds,
+                        locations = newLocationsList
+                    )
+                }
+            }
+            .launchIn(applicationScope)
+    }
+
+    fun setIsTracking(isTracking: Boolean) {
+        this.isTracking.value = isTracking
+    }
     fun startObservingLocation() {
         isObservingLocation.value = true
     }
@@ -33,4 +130,11 @@ class RunningTracker(
     fun stopObservingLocation() {
         isObservingLocation.value = false
     }
+}
+
+private fun <T> List<List<T>>.replaceLast(replacement: List<T>): List<List<T>> {
+    if (this.isEmpty()) {
+        return listOf(replacement)
+    }
+    return this.dropLast(1) + listOf(replacement)
 }
