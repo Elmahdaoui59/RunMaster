68bb61c761b5b44b05b45c04bca065c5c94f4a0d
Elmahdaoui <abdelghani.elmahdaoui@adria-bt.com>
2024-10-21 16:26:45 +0100

    9.4 Finishing a run

--- core/domain/src/main/java/com/running/core/domain/location/Location.kt
+++ core/domain/src/main/java/com/running/core/domain/location/Location.kt
@@ -10,16 +10,16 @@ data class Location(
     val long: Double
 ) {
     fun distanceTo(other: Location): Float {
-        val latDistance = Math. toRadians (other. lat - lat)
-        val longDistance = Math. toRadians (other.long - long)
-        val a = sin( latDistance / 2) * sin( latDistance / 2) +
-        cos (Math.toRadians (lat)) * cos (Math.toRadians (other.lat)) *
-                sin( longDistance / 2) * sin( longDistance / 2)
-        val c = 2 * atan2 (sqrt(a), sqrt ( 1 - a))
-        return EARTH_RADIUS_METERS * c. toFloat ()
+        val latDistance = Math.toRadians(other.lat - lat)
+        val longDistance = Math.toRadians(other.long - long)
+        val a = sin(latDistance / 2) * sin(latDistance / 2) +
+                cos(Math.toRadians(lat)) * cos(Math.toRadians(other.lat)) *
+                sin(longDistance / 2) * sin(longDistance / 2)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return EARTH_RADIUS_METERS * c.toFloat()
     }
 
-    companion object{
+    companion object {
         private const val EARTH_RADIUS_METERS = 6_371_000
     }
 }
--- run/presentation/src/main/java/com/running/run/presentation/active_run/ActiveRunAction.kt
+++ run/presentation/src/main/java/com/running/run/presentation/active_run/ActiveRunAction.kt
@@ -17,4 +17,5 @@ sealed interface ActiveRunAction {
         val showNotificationPermissionRationale: Boolean
     ): ActiveRunAction
     data object DismissRationaleDialog: ActiveRunAction
+    class OnRunProcessed(val mapPictureBytes: ByteArray): ActiveRunAction
 }
